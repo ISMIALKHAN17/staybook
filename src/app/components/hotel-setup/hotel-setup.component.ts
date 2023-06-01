6957cde649b9e44f058a1d1972af2db70b2d9dee
shani9286 <arsalanrazzaq613@hotmail.com>
2023-06-01 14:32:37 +0500

    Hotel Setup work Done

--- src/app/components/hotel-setup/hotel-setup.component.ts
+++ src/app/components/hotel-setup/hotel-setup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,AbstractControl } from '@angular/forms';
+import {RequestService} from '../../services/request.service'
 interface Image {
   url: string;
 }
@@ -11,11 +12,16 @@ interface Image {
 export class HotelSetupComponent {
   images: Image[] = [];
   hotelForm:any;
-
-  constructor(private formBuilder: FormBuilder) { }
+  amenity_list:any;
+  selectedAmenities: any[] = [];
+  submitted:boolean = false;
+  states_list:any;
+  cities_list:any;
+  constructor(private formBuilder: FormBuilder,private api:RequestService) { }
 
   onFileChange(event: any) {
-    const file = event.target.files[0];
+    for(let i = 0; i< event.target.files.length;i++){
+    const file = event.target.files[i];
     const reader = new FileReader();
 
     reader.onload = (e: any) => {
@@ -25,54 +31,93 @@ export class HotelSetupComponent {
 
     reader.readAsDataURL(file);
   }
+  }
   removeImage(index: number) {
     this.images.splice(index, 1);
   }
   ngOnInit(): void {
+    this.aminities();
+    this.states();
     this.hotelForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       contact: ['', Validators.required],
       rooms: ['', Validators.required],
-      addressLine1: ['', Validators.required],
-      addressLine2: [''],
-      country: ['', Validators.required],
-      state: ['', Validators.required],
-      city: ['', Validators.required],
+      address_line_1: ['', Validators.required],
+      address_line_2: [''],
+      country_id: ['', Validators.required],
+      state_id: ['', Validators.required],
+      city_id: ['', Validators.required],
       postalCode: ['', Validators.required],
-      airportShuttleOptions: this.formBuilder.group({
-        free: [false],
-        paid: [false],
-        airportDropOff: [false],
-        airportPickUp: [false]
-      }),
-      barOptions: this.formBuilder.group({
-        hugeBar: [false],
-        happyHour: [false],
-        miniBar: [false],
-        poolBar: [false]
-      }),
-      freeWifiOptions: this.formBuilder.group({
-        freeAllAround: [false],
-        freeInPublicAreas: [false],
-        paidInRoom: [false],
-        freeInRooms: [false]
-      }),
-      smokingOptions: this.formBuilder.group({
-        smokingGalleries: [false],
-        notAllowed: [false],
-        allowed: [false]
-      })
+      hotel_amenities: [''],
+      images:[''],
+      user_id:'1'
+
     });
-    
+
 }
 
 saveForm() {
+  this.submitted = true;
+  this.hotelForm.patchValue({images:this.images})
   if (this.hotelForm.valid) {
-    console.log(this.hotelForm.value);
+    this.api.post('property/store',this.hotelForm.value).subscribe((res:any)=>{
+      this.amenity_list = res.data;
+   });
   } else {
     console.log('Form is invalid');
+    console.log(this.hotelForm.value);
+
+
+
   }
 }
+
+aminities(){
+  this.api.post('amenity/list',true).subscribe((res:any)=>{
+   this.amenity_list = res.data;
+});
+}
+states(){
+  this.api.post('state/list',{"country_id":2}).subscribe((res:any)=>{
+   this.states_list = res.data;
+});
+}
+cities(id:any){
+  console.log(id.value);
+  this.api.post('city/list',{"state_id":id.value}).subscribe((res:any)=>{
+   this.cities_list = res.data;
+});
+}
+
+onCheckboxChange(event: any, list: any) {
+  if (event.target.checked) {
+    this.selectedAmenities.push({ "aminitiy_id" : list});
+  } else {
+
+    const index = this.selectedAmenities.findIndex((item) => item.aminitiy_id === list);
+
+
+    if (index !== -1) {
+      this.selectedAmenities.splice(index, 1);
+    }
+  }
+
+
+
+  const hotelAmenities = {
+    hotel_amenities: this.selectedAmenities
+  };
+
+
+  this.hotelForm.patchValue(hotelAmenities);
+
+
+}
+
+get f(): { [key: string]: AbstractControl } {
+  return this.hotelForm.controls;
+}
+
 
 }
--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -8,13 +8,13 @@ import { Observable } from 'rxjs';
 export class RequestService {
   // public global_url:any = "http://uat.technologenesis.com/public/api/";
 
-  private baseUrl = 'http://backend.staybook.pk/public/api/';
+  private baseUrl = 'http://backend.staybook.pk/public/api';
 
   constructor(private http: HttpClient) { }
 
   private getToken(): string {
     const token = localStorage.getItem('bearer_token');
-    return token !== null && token !== undefined ? token : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdWF0LnRlY2hub2xvZ2VuZXNpcy5jb20vcHVibGljL2FwaS9sb2dpbiIsImlhdCI6MTY4NDIzMTA0MCwiZXhwIjo0ODQxMjg3MDQwLCJuYmYiOjE2ODQyMzEwNDAsImp0aSI6ImFJbXlUY28wbDNqSzduS0siLCJzdWIiOiIyIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.-KEVhi2F02tgLNxhKplnAyBHZ0xIr_bsIv5m_7WCBXQ';
+    return token !== null && token !== undefined ? token : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2xvZ2luIiwiaWF0IjoxNjg0OTEwMzQ3LCJleHAiOjQ4Mzg1MTAzNDcsIm5iZiI6MTY4NDkxMDM0NywianRpIjoiak1EZU9jSUI0TmNIZUVodyIsInN1YiI6IjEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.XPwoM9GtXonSbssABY3ObBy0UrHjvkk6dmqxrezd48E';
   }
 
   private getHeaders(): HttpHeaders {
